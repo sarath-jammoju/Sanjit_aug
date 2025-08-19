aceb4ec736b3446dcecda2493422032c8e5f720a
sarath-jammoju <sarath.jammoju@sanjittechsolutions.in>
2025-08-19 10:07:18 +0530

    Fixed contactus page error

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import Services from '../Components/Services/Services.jsx'
 import WhyChoose from '../Components/WhyChoose/WhyChoose.jsx';
 import AboutUs from '../Components/AboutUs/AboutUs.jsx';
 import Review from '../Components/Reviewbox/Review.jsx';
-import ContactUs from '../Components/Contactus/ContactUs.jsx';
+import ContactUs from '../Components/ContactUs/ContactUs.jsx';
 import WhyChooseFaq from '../Components/whyChooseFaq/WhyChooseFaq.jsx';
 const Home = () => {
     return (
